096a8ac6356e5956b956263d5129593909810808
NMH85 <giuseppetasillo@gmail.com>
2023-02-06 12:42:50 +0100

    domestic animal

---
+++ src/model/IDomesticAnimal.ts
@@ -0,0 +1,13 @@
+export interface IDomesticAnimal {
+
+     
+    _id: number,
+    name: string,
+    type: "CAT" | "DOG",
+    breed: string,
+    birthDate: string,
+    created_at: string,
+    updated_at: string
+
+
+}
