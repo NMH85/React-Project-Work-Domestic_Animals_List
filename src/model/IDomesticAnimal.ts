b1724679085a8048305654bfa49fedda7a049838
NMH85 <giuseppetasillo@gmail.com>
2023-02-15 21:14:45 +0100

    modifche

--- src/model/IDomesticAnimal.ts
+++ src/model/IDomesticAnimal.ts
@@ -1,13 +1,16 @@
 export interface IDomesticAnimal {
-
-     
-    _id: number,
-    name: string,
-    type: "CAT" | "DOG",
-    breed: string,
-    birthDate: string,
-    created_at: string,
-    updated_at: string
-
-
-}
+    _id?: string;
+    name: string;
+    type: "CAT" | "DOG"| null;
+    breed: string;
+    birthDate: string;
+    imgUrl: string,
+    description: string,
+    pedigree: boolean,
+  }
+  
+  export interface IDomesticAnimalExtended extends IDomesticAnimal {
+    created_at: string;
+    updated_at: string;
+  }
+  
