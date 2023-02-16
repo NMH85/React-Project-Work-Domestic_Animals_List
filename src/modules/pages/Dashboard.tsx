948027c2ae2ba9cad320481f0c350b60391e89ad
NMH85 <giuseppetasillo@gmail.com>
2023-02-16 13:04:09 +0100

    modifiche 16

--- src/modules/pages/Dashboard.tsx
+++ src/modules/pages/Dashboard.tsx
@@ -3,7 +3,7 @@ export const Dashboard =()=>{
 return (
 <div className="Dashboard">
       <img
-        src="https://anziif.com/-/media/images/journal/2022/v45-issue-2/pet-insurance-1920x1080.jpg?rev=cc4ca291802641eea99504ba1b747609&w=860&hash=11D5797D0F77A717B619E392B5CE54C3"
+        src="https://www.mille-animali.com/wp/wp-content/uploads/2017/03/Cover-animali-1024x341.jpg"
         alt=""
       ></img>
       <h1>YOUR LIST OF DOMESTIC ANIMALS</h1>
