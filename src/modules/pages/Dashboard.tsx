ded6d1af1e3ff7a3ccad67003f247cc30ad07826
NMH85 <giuseppetasillo@gmail.com>
2023-02-16 12:58:01 +0100

    ultime modifiche

--- src/modules/pages/Dashboard.tsx
+++ src/modules/pages/Dashboard.tsx
@@ -1,4 +1,14 @@
 export const Dashboard =()=>{
 
-    return <div>Dashboard</div>
+return (
+<div className="Dashboard">
+      <img
+        src="https://anziif.com/-/media/images/journal/2022/v45-issue-2/pet-insurance-1920x1080.jpg?rev=cc4ca291802641eea99504ba1b747609&w=860&hash=11D5797D0F77A717B619E392B5CE54C3"
+        alt=""
+      ></img>
+      <h1>YOUR LIST OF DOMESTIC ANIMALS</h1>
+    </div>
+
+)
+
 }
